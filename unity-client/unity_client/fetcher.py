1f816fa76309c9b3a1a1cad918115446166e99af
Chetan Surpur <chetan51@gmail.com>
2015-05-18 16:03:02 -0700

    Started unity_client python module

---
+++ unity-client/unity_client/fetcher.py
@@ -0,0 +1,39 @@
+import json
+import time
+import urllib, urllib2
+
+
+
+class Fetcher(object):
+
+  def __init__(self,
+               url='http://localhost:8080/update'):
+    self.url = url
+
+    self.inputData = {}
+    self.lastPing = time.time()
+
+    self.timestep = -1
+    self.skippedTimestep = False
+
+
+  def sync(self):
+    values = {'inputData' : json.dumps(self.inputData)}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(self.url, data)
+    response = urllib2.urlopen(req)
+    outputData = json.loads(response.read())
+
+    newTimestep = self.timestep
+    if "timestep" in outputData:
+      newTimestep = outputData["timestep"]
+
+    if newTimestep == self.timestep:
+      return None
+
+    self.skippedTimestep = newTimestep > self.timestep + 1
+
+    self.lastPing = time.time()
+    self.timestep = newTimestep
+
+    return outputData
